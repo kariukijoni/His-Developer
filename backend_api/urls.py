3f006300cf776fc1d18242ae7ee6a5c58dcf5d71
kariukijoni <kariukijoni@gmail.com>
2023-11-14 11:21:22 +0300

    fixing models

--- backend_api/urls.py
+++ backend_api/urls.py
@@ -19,4 +19,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('nrb_data_api.urls')),
+    path('api-auth/', include('rest_framework.urls'))
 ]
--- nrb_data_api/models.py
+++ nrb_data_api/models.py
@@ -1,14 +1,31 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import RegexValidator
-from cities_light.models import Country
-from cities_light.models import City
+from cities_light.models import Country,City
 
 # Create your models here.
 
 class Address(models.Model):
     postal_address=models.TextField(max_length=100)
     postal_code=models.IntegerField()
+    date_created=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.postal_address} {self.postal_code}'
+
+
+class DisabledNationalCouncil(models.Model):
+    DISABILITY_STATUS=(
+        ('Hearing impairment','Hearing impairment'),
+        ('Cerebral palsy','Cerebral palsy'),
+        ('Vision impairment','Vision impairment'),
+        ('Autistic Spectrum Disorders','Autistic Spectrum Disorders')
+    )
+
+    reg_number=models.CharField(max_length=50,null=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    nature_of_disability=models.CharField(max_length=100,choices=DISABILITY_STATUS)
+    date_registered=models.DateTimeField(auto_now_add=True)
 
 
 class BioData(models.Model):
@@ -38,6 +55,11 @@ class BioData(models.Model):
     email_address=models.EmailField()
     alternative_contact_person=models.CharField(max_length=50)
     altenative_contact_person_telephone_number = models.CharField(null=True, max_length=100, validators=[RegexValidator(r'^\d{3}-\d{3}-\d{4}$')])
-
+    nature_of_disability=models.ForeignKey(DisabledNationalCouncil,max_length=100,null=True,default=None,blank=True,on_delete=models.SET_NULL)
+        
+    
+    def __str__(self):
+        return self.user
+    
 
   
